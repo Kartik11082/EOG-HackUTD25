fd819ca52c6359feb318868353cac61300657d60
Kartik11082 <karkerakartik27@gmail.com>
2025-11-09 03:29:43 -0600

    Frontend

---
+++ eogFrontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import PotionNetworkMap from '../components/Output2/PotionNetworkMap'
+
+function App() {
+
+  return (
+    <>
+      <PotionNetworkMap />
+    </>
+  )
+}
+
+export default App
