b84e84a5ecb47a614b7c57ae2760e8fff6dde656
Kartik11082 <karkerakartik27@gmail.com>
2025-11-09 04:17:19 -0600

    Fixed a lot of bugs in op2 and op3

--- eogFrontend/src/App.jsx
+++ eogFrontend/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
-import PotionNetworkMap from '../components/Output2/PotionNetworkMap'
+import PotionNetworkMap from '../components/Output2/CauldronWatch'
+import CauldronWatch from '../components/Output1/PotionNetworkMap'
+import DiscrepancyDashBoard from '../components/Output3/DiscrepancyDashBoard'
 
 function App() {
 
   return (
     <>
+      <CauldronWatch />
       <PotionNetworkMap />
+      <DiscrepancyDashBoard />
     </>
   )
 }
